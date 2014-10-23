3b167fc060bb2c3cd1fa3cc49ccf7a97aefd9757
suweining <1635737693@qq.com>
2014-10-23 20:10:02 +0800

    standardize shell name

---
+++ src/autoKrun.sh
@@ -0,0 +1,50 @@
+krun ../tests/unit/AbstractComponents.aadl  2> ../tests/logs/AbstractComponents.aadl.error
+
+krun ../tests/unit/BusComponentAccess.aadl  2> ../tests/logs/BusComponentAccess.aadl.error
+
+krun ../tests/unit/ComponentImplementations.aadl  2> ../tests/logs/ComponentImplementations.aadl.error
+
+krun ../tests/unit/ComponentTypes.aadl  2> ../tests/logs/ComponentTypes.aadl.error
+
+krun ../tests/unit/DataComponentAccess.aadl  2> ../tests/logs/DataComponentAccess.aadl.error
+
+krun ../tests/unit/Devices.aadl  2> ../tests/logs/Devices.aadl.error
+
+krun ../tests/unit/EndToEndFlows.aadl  2> ../tests/logs/EndToEndFlows.aadl.error
+
+krun ../tests/unit/FeatureGroupConnections.aadl  2> ../tests/logs/FeatureGroupConnections.aadl.error
+
+krun ../tests/unit/FeatureGroupsAndFeatureGroupTypes.aadl  2> ../tests/logs/FeatureGroupsAndFeatureGroupTypes.aadl.error
+
+krun ../tests/unit/FlowImplementations.aadl  2> ../tests/logs/FlowImplementations.aadl.error
+
+krun ../tests/unit/Memory.aadl  2> ../tests/logs/Memory.aadl.error
+
+krun ../tests/unit/ModesAndModeTransitions.aadl  2> ../tests/logs/ModesAndModeTransitions.aadl.error
+
+krun ../tests/unit/Packages.aadl  2> ../tests/logs/Packages.aadl.error
+
+krun ../tests/unit/PortConnections.aadl  2> ../tests/logs/PortConnections.aadl.error
+
+krun ../tests/unit/Ports.aadl  2> ../tests/logs/Ports.aadl.error
+
+krun ../tests/unit/PropertyDefinitions.aadl  2> ../tests/logs/PropertyDefinitions.aadl.error
+
+krun ../tests/unit/PropertyExpressions.aadl  2> ../tests/logs/PropertyExpressions.aadl.error
+
+krun ../tests/unit/PropertySetTest.aadl  2> ../tests/logs/PropertySetTest.aadl.error
+
+krun ../tests/unit/PropertyTypes.aadl  2> ../tests/logs/PropertyTypes.aadl.error
+
+krun ../tests/unit/Prototypes.aadl  2> ../tests/logs/Prototypes.aadl.error
+
+krun ../tests/unit/SubprogramAndSubprogramGroupAccess.aadl  2> ../tests/logs/SubprogramAndSubprogramGroupAccess.aadl.error
+
+krun ../tests/unit/SubprogramGroupsAndSubprogramGroupTypes.aadl  2> ../tests/logs/SubprogramGroupsAndSubprogramGroupTypes.aadl.error
+
+krun ../tests/unit/SubprogramsAndSubprogramCalls.aadl  2> ../tests/logs/SubprogramsAndSubprogramCalls.aadl.error
+
+krun ../tests/unit/SystemBinding.aadl  2> ../tests/logs/SystemBinding.aadl.error
+
+krun ../tests/unit/VirtualBuses.aadl  2> ../tests/logs/VirtualBuses.aadl.error
+
--- src/readme.md
+++ src/readme.md
@@ -1,1 +1,5 @@
 we want to rewrite the syntax of AADL on the K framework.The main file is the AADL.k ,and others are modules which AADL.k requies.Thus the file name is according to the AADL's module name.Inaddition,the version of AADL is AS5506A (AADL V2) released in September 19,2011.
+
+ps :
+autoKrun.sh is the file which could analyze all the exampiles in tests/unit.And the error massage save as *.error in tests/logs.
+You can use "./autoKrun.sh" to run this script;by the way,the path must  same as Maude's or Java's. 
